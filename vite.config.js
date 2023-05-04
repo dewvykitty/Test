dfda9823caddd5f261c68d8884bd31b22668206a
akkarapong2538@gmail.com <akkarapong2538@gmail.com>
2023-05-04 10:33:57 +0700

    change port

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 8000, // This is the port which we will use in docker
+    port: 5173, // This is the port which we will use in docker
   }
  })
